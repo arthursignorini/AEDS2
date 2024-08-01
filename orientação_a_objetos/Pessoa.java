b0901ab5f4608f12cb631aea227645e0f407750b
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-07-31 23:44:00 -0300

    Update Pessoa.java

--- orientação_a_objetos/Pessoa.java
+++ orientação_a_objetos/Pessoa.java
@@ -1,25 +1,30 @@
 import java.util.Scanner;
 
-public class Pessoa {
+class Pessoa {
     private String nome;
     private int idade;
 
-    public Pessoa (String nome, int idade) {
+    public Pessoa(String nome, int idade) {
         this.nome = nome;
         this.idade = idade;
     }
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getNome() {
         return nome;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void apresentar() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
@@ -33,9 +38,11 @@ class Aluno extends Pessoa {
         super(nome, idade);
         this.curso = curso;
     }
+
     public void setCurso(String curso) {
         this.curso = curso;
     }
+
     public String getCurso() {
         return curso;
     }
@@ -49,7 +56,7 @@ class Aluno extends Pessoa {
 }
 
 public class Main {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o seu nome: ");
         String nome = scanner.nextLine();
@@ -57,8 +64,10 @@ public class Main {
         String curso = scanner.nextLine();
         System.out.println("Digite a sua idade: ");
         int idade = scanner.nextInt();
+
         Aluno a1 = new Aluno(nome, idade, curso);
         Pessoa p1 = new Pessoa(nome, idade);
+
         a1.apresentar();
         p1.apresentar();
     }
