b2d649e1fee871522ea8c417feb23d1ce84edd7f
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-08-04 19:03:14 -0300

    Update ex1.c

--- nivelamento/iniciais/ex1.c
+++ nivelamento/iniciais/ex1.c
@@ -1,33 +1,59 @@
-#include<stdio.h>
-#include<stdbool.h>
+#include <stdio.h>
+#include <stdbool.h>
 
-bool conferir(int vet[])
-{
-    int x;
-    bool resp = false;
-    printf("Digite um numero: ");
-    scanf("%d",&x);
-    for(int i=0; i<5; i++){
-        if(x == vet[i]){
-            resp = true;
-            break;
+// Função de busca binária
+bool buscarBinaria(int vet[], int esquerda, int direita, int x) {
+    while (esquerda <= direita) {
+        int meio = esquerda + (direita - esquerda) / 2;
+
+        if (vet[meio] == x) {
+            return true;
+        }
+
+        if (vet[meio] < x) {
+            esquerda = meio + 1;
+        } else {
+            direita = meio - 1;
         }
     }
-    return resp;
+    return false;
 }
 
-int main()
-{
+// Função para conferir se o número está no vetor
+bool conferir(int vet[], int tamanho) {
+    int x;
+    printf("Digite um numero: ");
+    scanf("%d", &x);
+    
+    return buscarBinaria(vet, 0, tamanho - 1, x);
+}
+
+int main() {
     int vet[5];
-    for(int i=0; i<5; i++){
+    for (int i = 0; i < 5; i++) {
         printf("Digite um valor: ");
-        scanf("%d",&vet[i]);
+        scanf("%d", &vet[i]);
     }
 
-    bool resposta = conferir(vet);
-    if(resposta){
-        printf("Existe um numero igual no vetor");
-    } else {
-        printf("Nao existe um numero igual no vetor");
+    // Ordenar o vetor em ordem crescente
+    int temp;
+    for (int i = 0; i < 5 - 1; i++) {
+        for (int j = 0; j < 5 - i - 1; j++) {
+            if (vet[j] > vet[j + 1]) {
+                temp = vet[j + 1];
+                vet[j + 1] = vet[j];
+                vet[j] = temp;
+            }
+        }
     }
+
+    // Conferir se o número está no vetor
+    bool resposta = conferir(vet, 5);
+    if (resposta) {
+        printf("Existe um numero igual no vetor\n");
+    } else {
+        printf("Nao existe um numero igual no vetor\n");
+    }
+
+    return 0;
 }
