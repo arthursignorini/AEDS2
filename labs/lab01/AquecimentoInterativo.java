0804bbc56b89dade082df3cf65c0446082d90bf8
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-05 19:24:55 -0300

    commit

---
+++ labs/lab01/AquecimentoInterativo.java
@@ -0,0 +1,26 @@
+package labs.lab01;
+
+import java.util.Scanner;
+
+public class AquecimentoInterativo {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite uma string: ");
+        String string = scanner.nextLine();
+        int num = contar(string);
+        System.out.println("Número de caracteres maiúsculos: " + num);
+    }
+
+    public static int contar(String string) {
+        int tam = string.length();
+        int cont = 0;
+        for (int i = 0; i < tam; i++) {
+            if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') {
+                cont++;
+            }
+        }
+        return cont;
+    }
+}
