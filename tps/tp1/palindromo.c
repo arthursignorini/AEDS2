20ac715ab68ef0c32801cc85618d8c2ee0a317c7
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-06 17:46:10 -0300

    add

---
+++ tps/tp1/palindromo.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdbool.h>
+
+
+bool conferir(char string[]){
+    int x = strlen(string);
+    for(int i=0; i<x/2; i++){
+        if(string[i] != string[x-1-i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+    char string[50];
+    printf("Digite um string: ");
+    fgets(string, 50, stdin);
+    string[strcspn(string, "\n")] = '\0';
+    
+    if(conferir(string)){
+        printf("A frase eh um palindromo");
+    } else {
+        printf("A frase nao eh um palindromo");
+    }
+
+}
