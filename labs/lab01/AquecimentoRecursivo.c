b896b78f77b77593e4492a0f25221f4826a96b6e
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-14 19:53:51 -0300

    add

--- labs/lab01/AquecimentoRecursivo.c
+++ labs/lab01/AquecimentoRecursivo.c
@@ -1,22 +1,32 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int contar(char string[], int i){
-    if(string[i] == '\0') {
+int contar(char string[], int i) {
+    if (string[i] == '\0') {
         return 0;
     } else {
-        if(string[i] >= 'A' && string[i] <= 'Z'){
-            return 1 + contar(string, i+1);
+        if (string[i] >= 'A' && string[i] <= 'Z') {
+            return 1 + contar(string, i + 1);
         } else {
-            return contar(string, i+1);
+            return contar(string, i + 1);
         }
     }
 }
 
 int main() {
-    char string[50];
-    printf("Digite uma frase: ");
-    fgets(string, 50, stdin);
-    int num = contar(string, 0);
-    printf("%d",num);
+
+    char palavra[1000];
+    while (1) 
+    {
+        scanf(" %[^\n]", palavra);
+
+        if (strcmp(palavra, "FIM") == 0) {
+            break;
+        } else {
+            int num = contar(palavra, 0);
+            printf("%d\n", num); 
+        }
+    }
+    
+    return 0; 
 }
