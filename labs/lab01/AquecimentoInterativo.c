121ce96e434b96b399387926be926aff3d909899
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-14 19:57:11 -0300

    add

--- labs/lab01/AquecimentoInterativo.c
+++ labs/lab01/AquecimentoInterativo.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
-int contar (char string[]){
+int contar(char string[]) {
     int tam = strlen(string);
     int cont = 0;
-    for(int i=0; i<tam; i++){
+    for(int i = 0; i < tam; i++) {
         if(string[i] >= 'A' && string[i] <= 'Z') {
             cont++;
         }
@@ -12,10 +12,23 @@ int contar (char string[]){
     return cont;
 }
 
-int main(){
-    char string[50];
-    printf("Digite uma string: ");
-    fgets(string, 50, stdin);
-    int x = contar(string);
-    printf("%d",x);
+int main() {
+
+    char palavra[1000];
+    while(1){
+        scanf(" %[^\n]", palavra);
+
+        if (strcmp(palavra, "FIM") == 0) {
+            break;
+        } else {
+            char string[50];
+        printf("Digite uma string: ");
+        fgets(string, 50, stdin);
+        string[strcspn(string, "\n")] = 0;
+
+        int x = contar(string);
+        printf("%d\n", x);
+        }
+    }
+    return 0;
 }
