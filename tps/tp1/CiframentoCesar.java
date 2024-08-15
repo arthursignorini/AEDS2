c42214e0059364f14e4491ec5a983a21bd2d85b6
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-14 22:14:56 -0300

    Update CiframentoCesar.java

--- tps/tp1/CiframentoCesar.java
+++ tps/tp1/CiframentoCesar.java
@@ -3,31 +3,34 @@ package tps.tp1;
 import java.util.Scanner;
 
 public class CiframentoCesar {
-    public static String criptografar(String palavra) {
-        if (palavra.isEmpty()) {
-            return "";
+    public static String criptografar(String str, int chave) {
+        String cifrado = "";
+        int size = str.length();
+
+        for (int i = 0; i < size; i++) {
+            char caractere = str.charAt(i); // pegando caracter posicao por posicao
+            int codigo = (int) caractere; // casting de char para int
+    
+            // Verifica se o caractere é um caractere imprimível da tabela ASCII
+            if (codigo >= 32 && codigo <= 126) {
+                // Aplica a cifra de César para o caractere
+                codigo = (codigo - 32 + chave) % 95 + 32;
+                cifrado += (char) codigo;
+            } else {
+                // Caso o caractere não seja um caractere imprimível, mantém o mesmo
+                cifrado += caractere;
+            }
         }
-
-        char primeiroChar = palavra.charAt(0);
-        int letra = (int) primeiroChar;
-
-        letra = letra + 3;
-
-        if (letra > 126) {
-            letra = 32 + (letra - 127);
-        }
-
-        char charCriptografado = (char) letra;
-
-        return charCriptografado + criptografar(palavra.substring(1));
+    
+        return cifrado;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int chave = 3;
         String string;
         while(!(string = scanner.nextLine()).equals("FIM")){
-                String palavraCriptografada = criptografar(string);
+                String palavraCriptografada = criptografar(string, chave);
                 System.out.println(palavraCriptografada);
         }
         scanner.close();
