d7e78dcb9d0017dff99f3616531125b6a78944bf
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-15 21:30:17 -0300

    add

--- tps/tp1/CiframentoCesar.java
+++ tps/tp1/CiframentoCesar.java
@@ -8,23 +8,18 @@ public class CiframentoCesar {
         int size = str.length();
 
         for (int i = 0; i < size; i++) {
-            char caractere = str.charAt(i); // pegando caracter posicao por posicao
-            int codigo = (int) caractere; // casting de char para int
-    
-            // Verifica se o caractere é um caractere imprimível da tabela ASCII
+            char caractere = str.charAt(i); 
+            int codigo = (int) caractere; 
             if (codigo >= 32 && codigo <= 126) {
-                // Aplica a cifra de César para o caractere
+               
                 codigo = (codigo - 32 + chave) % 95 + 32;
                 cifrado += (char) codigo;
             } else {
-                // Caso o caractere não seja um caractere imprimível, mantém o mesmo
                 cifrado += caractere;
             }
         }
-    
         return cifrado;
     }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int chave = 3;
