9bc4383e0a922b33dc1554809f42accc9a788723
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-15 21:33:06 -0300

    edit

--- tps/tp1/CiframentoCesar.java
+++ tps/tp1/CiframentoCesar.java
@@ -3,32 +3,32 @@ package tps.tp1;
 import java.util.Scanner;
 
 public class CiframentoCesar {
-    public static String criptografar(String str, int chave, int i) {
-        if (i == str.length()) {
-            return "";
-        }
-        
-        char caractere = str.charAt(i); 
-        int codigo = (int) caractere; 
-        
-        if (codigo >= 32 && codigo <= 126) {
-            codigo = (codigo - 32 + chave) % 95 + 32;
-            return (char) codigo + criptografar(str, chave, i + 1);
-        } else {
-            return caractere + criptografar(str, chave, i + 1);
-        }
-    }
+    public static String criptografar(String str, int chave) {
+        String cifrado = "";
+        int size = str.length();
 
+        for (int i = 0; i < size; i++) {
+            char caractere = str.charAt(i); 
+            int codigo = (int) caractere; 
+            if (codigo >= 32 && codigo <= 126) {
+               
+                codigo = (codigo - 32 + chave) % 95 + 32;
+                cifrado += (char) codigo;
+            } else {
+                cifrado += caractere;
+            }
+        }
+        return cifrado;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int chave = 3;
         String string;
-        
         while(!(string = scanner.nextLine()).equals("FIM")){
-            String palavraCriptografada = criptografar(string, chave, 0);
-            System.out.println(palavraCriptografada);
+                String palavraCriptografada = criptografar(string, chave);
+                System.out.println(palavraCriptografada);
         }
-        
         scanner.close();
     }     
 }
+
--- tps/tp1/CiframentoCesarRec.java
+++ tps/tp1/CiframentoCesarRec.java
@@ -1,5 +1,34 @@
 package tps.tp1;
 
+import java.util.Scanner;
+
 public class CiframentoCesarRec {
-    
+    public static String criptografar(String str, int chave, int i) {
+        if (i == str.length()) {
+            return "";
+        }
+        
+        char caractere = str.charAt(i); 
+        int codigo = (int) caractere; 
+        
+        if (codigo >= 32 && codigo <= 126) {
+            codigo = (codigo - 32 + chave) % 95 + 32;
+            return (char) codigo + criptografar(str, chave, i + 1);
+        } else {
+            return caractere + criptografar(str, chave, i + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int chave = 3;
+        String string;
+        
+        while(!(string = scanner.nextLine()).equals("FIM")){
+            String palavraCriptografada = criptografar(string, chave, 0);
+            System.out.println(palavraCriptografada);
+        }
+        
+        scanner.close();
+    }     
 }
