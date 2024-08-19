a084e1487bccb01c483abb5febfcc3ea8b4c4e5c
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-19 19:52:04 -0300

    Update Combinador.java

--- labs/lab02/Combinador.java
+++ labs/lab02/Combinador.java
@@ -3,37 +3,41 @@ package labs.lab02;
 import java.util.Scanner;
 
 public class Combinador {
-    
-    public static void main(String[]args){
 
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
 
-        System.out.print("Digite uma string: ");
-        String string1 = scanner.nextLine();
+        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Digite outra string: ");
-        String string2 = scanner.nextLine();
-
-        int tam1 = string1.length();
-        int tam2 = string2.length();
-
-        if(tam1 < tam2) {
-            for(int i=0; i<tam1; i++){
-                System.out.print(string1.charAt(i));
-                System.out.print(string2.charAt(i));
+        while (true) {
+            String string1 = sc.next();
+            String string2 = sc.next();
+            
+          
+            if (string1.equals(" ") || string2.equals(" ")) {
+                break;
             }
-            for(int i=tam1; i<tam2; i++){
-                System.out.print(string2.charAt(i));
-            } 
-        } else {
-            for(int i=0; i<tam2; i++){
-                System.out.print(string1.charAt(i));
-                System.out.print(string2.charAt(i));
+
+            int tam1 = string1.length();
+            int tam2 = string2.length();
+
+            if (tam1 < tam2) {
+                for (int i = 0; i < tam1; i++) {
+                    System.out.print(string1.charAt(i));
+                    System.out.print(string2.charAt(i));
+                }
+                for (int i = tam1; i < tam2; i++) {
+                    System.out.print(string2.charAt(i));
+                }
+            } else {
+                for (int i = 0; i < tam2; i++) {
+                    System.out.print(string1.charAt(i));
+                    System.out.print(string2.charAt(i));
+                }
+                for (int i = tam2; i < tam1; i++) {
+                    System.out.print(string1.charAt(i));
+                }
             }
-            for(int i=tam2; i<tam1; i++){
-                System.out.print(string1.charAt(i));
-            } 
         }
-        scanner.close();
+        sc.close();
     }
 }
