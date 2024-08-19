7983a8e86815191dfe81de8aba4baca39c328a00
Arthur Signorini Miranda <arthursigmiranda@gmail.com>
2024-08-19 20:56:53 -0300

    Update Espelho.java

--- labs/lab02/Espelho.java
+++ labs/lab02/Espelho.java
@@ -7,45 +7,71 @@ public class Espelho {
 
         Scanner sc = new Scanner(System.in);
 
-        while (true) {
-            String input1 = sc.next();
-            String input2 = sc.next();
+        int[] vet = new int[100];
+        int cont = 0;
 
-            if (input1.equals(" ") || input2.equals(" ")) {
+        String input1;
+        String input2;
+
+        while (true) {
+            input1 = sc.next();
+
+            if (input1.equals(" ")) {
                 break;
             }
 
-            int aux = 0;
+            input2 = sc.next();
+
+            if (input2.equals(" ")) {
+                break;
+            }
 
             int num1 = Integer.parseInt(input1);
             int num2 = Integer.parseInt(input2);
+
+            int aux;
 
             if (num1 > num2) {
                 aux = num2;
                 while (aux <= num1) {
                     System.out.print(aux);
+                    vet[cont] = aux;
+                    cont++;
                     aux++;
                 }
-                aux--;
-                while (aux >= num2) {
-                    System.out.print(aux);
-                    aux--;
-                }
-            } else if (num2 > num1) {
+                printar(cont, vet);
+            } else {
                 aux = num1;
                 while (aux <= num2) {
                     System.out.print(aux);
+                    vet[cont] = aux;
+                    cont++;
                     aux++;
                 }
-                aux--;
-                while (aux >= num1) {
-                    System.out.print(aux);
-                    aux--;
-                }
+                printar(cont, vet);
             }
+            System.out.println();
 
+            // Esvaziar o vetor
+            limparVetor(vet);
+            cont = 0; // Resetar o contador
         }
 
         sc.close();
+    }
+
+    public static void printar(int cont, int[] vet) {
+        for (int i = cont - 1; i >= 0; i--) {
+            String numeroStr = Integer.toString(vet[i]);
+            // Inverter o número e imprimir
+            String numeroEspelhado = new StringBuilder(numeroStr).reverse().toString();
+            System.out.print(numeroEspelhado);
+        }
+    }
+
+    public static void limparVetor(int[] vet) {
+        for (int i = 0; i < vet.length; i++) {
+            vet[i] = 0; // Define todos os valores do vetor como zero
+        }
     }
 }
