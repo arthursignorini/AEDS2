6864638b7c5794a1a5971ac6bfe4438d601f4397
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-08-27 20:18:46 -0300

    Update AlgebraBooleanaRec.java

--- tps/tp1/AlgebraBooleanaRec.java
+++ tps/tp1/AlgebraBooleanaRec.java
@@ -3,152 +3,126 @@ package tps.tp1;
 import java.util.Scanner;
 
 public class AlgebraBooleanaRec {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in); 
-        String entrada = "";
 
-        do {
-            entrada = scanner.nextLine(); 
-            if (!isEquals(entrada, "0")) {
-                if (solveEquation(entrada)) {
-                    System.out.println("1"); 
-                } else {
-                    System.out.println("0"); 
-                }
+    // Converte um caractere numérico para um número inteiro
+    public static int parseInt(char str) {
+        return str - '0';
+    }
+
+    // Converte um caractere '0' ou '1' para um booleano
+    public static boolean parseBoolean(char str) {
+        return str == '1';
+    }
+
+    // Extrai uma parte de uma string
+    public static String substring(String str, int start, int end) {
+        return str.substring(start, end);
+    }
+
+    // Substitui uma parte da string
+    public static String replace(String str, int start, int end, String subReplace) {
+        return str.substring(0, start) + subReplace + str.substring(end);
+    }
+
+    // Extrai os argumentos de uma expressão lógica
+    public static char[] getArgs(String str, int index) {
+        int count = 0;
+        // esse primeiro conta quantos argumentos lógicos tem dentro do parenteses
+        for (int i = index + 1; str.charAt(i) != ')'; i++) {
+            if (str.charAt(i) == '0' || str.charAt(i) == '1') {
+                count++;
             }
-        } while (!isEquals(entrada, "0"));
-
-        scanner.close(); 
-    }
-
-    public static String treatment(String input) {
-        String expressao = "";
-        int n = 0;
-
-        n = input.charAt(0) - 48; // capturar quantos bits serão utilizados
-
-        expressao = removeBlank(input); // remover espaços em branco
-        expressao = replaceAll("and", 'a', expressao); // trocar "and" por 'a'
-        expressao = replaceAll("or", 'o', expressao); // trocar "or" por 'o'
-        expressao = replaceAll("not", 'n', expressao); // trocar "not" por 'n'
-
-        // trocar 'A', 'B' e 'C' por seus respectivos bits
-        for (int x = 0; x < n; x = x + 1) {
-            char c = (char) ('A' + x);
-            char newC = expressao.charAt(x + 1);
-            expressao = replaceAll(c, newC, expressao);
+        }
+        // nesse loop ele extrai os argumentos e os armazena dentro de um array
+        char[] args = new char[count];
+        int j = 0;
+        for (int i = index + 1; str.charAt(i) != ')'; i++) {
+            if (str.charAt(i) == '0' || str.charAt(i) == '1') {
+                args[j++] = str.charAt(i);
+            }
         }
 
-        // pegar apenas a expressão lógica
-        expressao = subString(expressao, n + 1, expressao.length());
-
-        return (expressao);
+        return args;
     }
 
-    public static String solveExp(String input) {
-        int inputLen = input.length();
-        char operation = input.charAt(0);
-        boolean flag = true;
-        switch (operation) {
-            case 'a':
-                flag = true;
-                for (int y = 2; y < inputLen && flag; y = y + 1) {
-                    if (input.charAt(y) == '0') {
-                        flag = false;
-                        return ("0");
-                    }
-                }
-                return ("1");
-            case 'o':
-                flag = false;
-                for (int y = 2; y < inputLen && !flag; y = y + 1) {
-                    if (input.charAt(y) == '1') {
-                        flag = true;
-                        return ("1");
-                    }
-                }
-                return ("0");
-            case 'n':
-                if (input.charAt(2) == '0') {
-                    return ("1");
-                } else {
-                    return ("0");
-                }
-            default:
-                return ("");
+    // Operação NOT
+    public static char not(char arg) {
+        return arg == '0' ? '1' : '0';
+    }
+
+    // Operação AND
+    public static char and(char[] args) {
+        for (char arg : args) {
+            if (arg == '0') return '0';
         }
+        return '1';
     }
 
-    public static boolean solveEquation(String input) {
-        String expressao = treatment(input);
-        return (solveEquationRec(expressao));
-    }
-
-    public static boolean solveEquationRec(String input) {
-        if (input.contains("(")) {
-            int start = input.lastIndexOf('(');
-            int end = input.indexOf(')', start);
-
-            String subExp = subString(input, start - 1, end + 1);
-            String resultString = solveExp(subExp);
-
-            input = subString(input, 0, start - 1) +
-                    resultString +
-                    subString(input, end + 1, input.length());
-
-            return (solveEquationRec(input));
-        } else {
-            return (isEquals(input, "1"));
+    // Operação OR
+    public static char or(char[] args) {
+        for (char arg : args) {
+            if (arg == '1') return '1';
         }
+        return '0';
     }
 
-    public static boolean isEquals(String obj1, String obj2) {
-        boolean result = true;
-        if (obj1.length() != obj2.length()) {
-            result = false;
-        } else {
-            for (int x = 0; x < obj1.length() && result; x = x + 1) {
-                if (obj1.charAt(x) != obj2.charAt(x)) {
-                    result = false;
+    // Resolve a expressão lógica - substitui partes da equação pelos seus resultados
+    public static String solveEquation(String str) throws Exception {
+        for (int i = str.length() - 1; i >= 0; i--) {
+            if (str.charAt(i) == '(') {
+                char op = str.charAt(i - 1);
+                char[] args = getArgs(str, i);
+                char result;
+                switch (op) { // vai ler a ultima letra das palavras
+                    case 'd': // AND
+                        result = and(args);
+                        str = replace(str, i - 3, str.indexOf(')', i) + 1, Character.toString(result));
+                        break;
+                    case 't': // NOT
+                        result = not(args[0]);
+                        str = replace(str, i - 3, i + 3, Character.toString(result));
+                        break;
+                    case 'r': // OR
+                        result = or(args);
+                        str = replace(str, i - 2, str.indexOf(')', i) + 1, Character.toString(result));
+                        break;
+                    default:
+                        throw new Exception("Operação desconhecida.");
                 }
             }
         }
-        return (result);
+        return str;
     }
 
-    public static String subString(String s, int start, int end) {
-        String result = "";
-        for (int x = start; x < end && x < s.length(); x = x + 1) {
-            result += s.charAt(x);
-        }
-        return (result);
-    }
+    // Função principal
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        String str;
 
-    public static String removeBlank(String input) {
-        String result = "";
-        for (int x = 0; x < input.length(); x = x + 1) {
-            if (input.charAt(x) != ' ') {
-                result = result + input.charAt(x);
+        while (!(str = sc.nextLine()).equals("0")) {
+            int qnt = parseInt(str.charAt(0));
+            boolean[] values = new boolean[qnt];
+
+            // Armazena os valores das variáveis
+            for (int i = 0; i < qnt; i++) {
+                values[i] = parseBoolean(str.charAt(2 + 2 * i));
             }
-        }
-        return (result);
-    }
 
-    public static String replaceAll(Object base, char newChar, String input) {
-        String result = "";
-        int inputLen = input.length();
-        String baseString = base.toString();
-        int baseLen = baseString.length();
-        for (int x = 0; x < inputLen; x = x + 1) {
-            boolean ok = x < inputLen - baseLen + 1;
-            boolean match = isEquals(subString(input, x, x + baseLen), baseString);
-            if (ok && match) {
-                result += newChar;
-                x += baseLen - 1;
-            } else {
-                result += input.charAt(x);
+            // Substitui variáveis na expressão
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == 'A') {
+                    str = replace(str, i, i + 1, values[0] ? "1" : "0");
+                } else if (str.charAt(i) == 'B') {
+                    str = replace(str, i, i + 1, values[1] ? "1" : "0");
+                } else if (str.charAt(i) == 'C') {
+                    str = replace(str, i, i + 1, values[2] ? "1" : "0");
+                }
             }
+
+            // Resolve a expressão e imprime o resultado
+            System.out.println(solveEquation(str));
         }
-        return (result);
+
+        sc.close();
     }
 }
