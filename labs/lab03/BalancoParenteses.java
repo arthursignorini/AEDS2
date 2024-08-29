3ec2fa8e20c2280ffcd919bae303f7f394dc3fd1
Arthur Signorini Miranda <1511453@sga.pucminas.br>
2024-08-29 07:40:19 -0300

    Update BalancoParenteses.java

--- labs/lab03/BalancoParenteses.java
+++ labs/lab03/BalancoParenteses.java
@@ -1,38 +1,20 @@
-package labs.lab03;
-
 import java.util.Scanner;
 
 public class BalancoParenteses {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         String str;
 
         while (!(str = sc.nextLine()).equals("FIM")) {
 
-            
-            int cont1 = 0, cont2 = 0;
-
-            for (int i = 0; i < str.length(); i++) {
-                if (str.charAt(i) == '(') {
-                    cont1++;
-                }
-                if (str.charAt(i) == ')') {
-                    cont2++;
-                }
-            }
-            if (cont1 != cont2) {
-                System.out.println("incorreto");
+            if (conferir(str)) {
+                System.out.println("correto");
             } else {
-                if (conferir(str)) {
-                    System.out.println("correto");
-                } else {
-                    System.out.println("incorreto");
-                }
+                System.out.println("incorreto");
             }
         }
-
         sc.close();
     }
 
