f64b23519aea09e03949023d8cdc29acaf235ff9
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-02 17:59:41 -0300

    add

--- provas_resolvidas/p1/ex01.java
+++ provas_resolvidas/p1/ex01.java
@@ -1,5 +1,25 @@
 package provas_resolvidas.p1;
 
 public class ex01 {
-    
+    public static void main(String[] args) {
+
+        String str = "ola meu bom homem";
+        if(isPalindromo(str)) {
+            System.out.println("SIM");
+        } else {
+            System.out.println("NAO");
+        }
+    }
 }
+
+class Exercicio {
+    boolean isPalindromo(String s) {
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
