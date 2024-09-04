7b9e7f82f73d5c6799f3c4b26e207c2376b77544
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-04 17:02:20 -0300

    Create teste.java

---
+++ tads/teste.java
@@ -0,0 +1,11 @@
+public class teste {
+    public static void main(String[] args) throws Exception {
+        listaSequencial lista = new listaSequencial(5);
+        lista.inserirInicio(1);
+        lista.inserirInicio(2);
+        lista.inserirInicio(3);
+        lista.inserirInicio(4);
+        lista.inserirInicio(5);
+        lista.mostrar();
+    }
+}
