d442aef8cfe3f3ba71b17012af939c7bf90fee5b
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-11 16:05:41 -0300

    Create Dicionario_de_Andy.java

---
+++ beecrowd/Dicionario_de_Andy.java
@@ -0,0 +1,27 @@
+package beecrowd;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Dicionario_de_Andy {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> palavras = new ArrayList<>(); 
+        
+        while (sc.hasNext()) {
+            String palavra = sc.next().toLowerCase();
+            palavras.add(palavra);  
+        }
+
+        
+        Collections.sort(palavras);
+
+        
+        for (String palavra : palavras) {
+            System.out.println(palavra);
+        }
+
+        sc.close();  
+    }
+}
