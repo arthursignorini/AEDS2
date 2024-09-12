4b776dae1c60dfb288a7e41a0eca5f7f133089a0
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-12 11:55:06 -0300

    Update Time_Duendes.java

--- Time_Duendes.java
+++ Time_Duendes.java
@@ -9,47 +9,76 @@ public class Time_Duendes {
         nome = n;
         idade = i;
     }
-
-    @Override
-    public String toString() {
-        return nome + " " + idade;
-    }
-}
-
-class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int qntd = sc.nextInt();
-        sc.nextLine();
-        ArrayList<Time_Duendes> duendes = new ArrayList<>(qntd);
+        ArrayList<Time_Duendes> duendes = new ArrayList<>();
+        int qntd;
 
-        for(int i = 0; i < qntd; i++) {
-            String nome = sc.nextLine();
+        qntd = sc.nextInt();
+        sc.nextLine(); 
+
+        // ler as informações dos duendes
+        for (int i = 0; i < qntd; i++) {
+            String nome = sc.next();
             int idade = sc.nextInt();
-            sc.nextLine();
+            sc.nextLine(); 
+
             Time_Duendes duende = new Time_Duendes(nome, idade);
             duendes.add(duende);
         }
 
-        bubblesort(duendes, qntd);
+        // ordenar os duendes - idades
+        bubbleSort(duendes);
 
-        for(int i = 0; i < qntd; i++) {
-            System.out.println(duendes.get(i));
-        }
-
+        // separando os duendes por times
         int times = qntd / 3;
-        
+        int grupo = 0;
+        while (grupo < times) {
+            System.out.println("Time " + (grupo + 1));
+            
+            int index = grupo;
+            // líder
+            if (index < duendes.size()) {
+                System.out.println(duendes.get(index));
+                index += times;
+            }
+            
+            // entregadores
+            if (index < duendes.size()) {
+                System.out.println(duendes.get(index));
+                index += times;
+            }
+            
+            // pilotos
+            if (index < duendes.size()) {
+                System.out.println(duendes.get(index));
+                index += times;
+            }
+            System.out.println();
+            grupo++;
+        }
+        sc.close();
     }
 
-    public static void bubblesort(ArrayList<Time_Duendes> duendes, int qntd) {
-        for(int i = 0; i < qntd - 1; i++) {
-            for(int j = 0; j < qntd - 1 - i; j++) {
-                if(duendes.get(j).idade > duendes.get(j + 1).idade) {
-                    Time_Duendes temp = duendes.get(j + 1);
-                    duendes.set(j + 1, duendes.get(j));
-                    duendes.set(j, temp);
+    // ordenar os mais velhos
+    public static void bubbleSort(ArrayList<Time_Duendes> duendes) {
+        int n = duendes.size();
+        boolean trocou;
+        do {
+            trocou = false;
+            for (int i = 0; i < n - 1; i++) {
+                Time_Duendes d1 = duendes.get(i);
+                Time_Duendes d2 = duendes.get(i + 1);
+                
+                // verificar se uma idade é menor e se forem iguais, ver a ordem alfabetica
+                if (d1.idade < d2.idade || (d1.idade == d2.idade && d1.nome.compareTo(d2.nome) > 0)) {
+                    // troca os objetos
+                    duendes.set(i, d2);
+                    duendes.set(i + 1, d1);
+                    trocou = true;
                 }
             }
-        }
+            n--; 
+        } while (trocou);
     }
 }
