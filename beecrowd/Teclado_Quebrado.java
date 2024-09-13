770e02c114e73df0f0f854a07f18b2d6e018f855
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-13 11:15:48 -0300

    Update Teclado_Quebrado.java

--- beecrowd/Teclado_Quebrado.java
+++ beecrowd/Teclado_Quebrado.java
@@ -1,5 +1,37 @@
 package beecrowd;
 
+import java.util.Scanner;
+
 public class Teclado_Quebrado {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            String str = sc.nextLine();
+
+            StringBuilder inicio = new StringBuilder();
+            StringBuilder fim = new StringBuilder();
+
+            boolean addInicio = false;
+
+            for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+
+                if (ch == '[') {
+                    addInicio = true; 
+                } else if (ch == ']') {
+                    addInicio = false;  
+                } else {
+                    if (addInicio) {
+                        inicio.insert(0, ch);  
+                    } else {
+                        fim.append(ch);  
+                    }
+                }
+            }
+            inicio.append(fim);
+            System.out.println(inicio.toString());
+        }
+        sc.close();
+    }
 }
