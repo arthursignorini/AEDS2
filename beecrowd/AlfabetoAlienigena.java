300b254542d7a925d59ba648c1ce9543cd27b14b
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-18 14:57:22 -0300

    Update AlfabetoAlienigena.java

--- beecrowd/AlfabetoAlienigena.java
+++ beecrowd/AlfabetoAlienigena.java
@@ -1,33 +1,37 @@
 package beecrowd;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class AlfabetoAlienigena {
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-
-        String alfa;
-        String str;
-        
-        alfa = sc.next();
-        str = sc.next();
-
+    public static boolean comparar(String alfa, String str, int n1, int n2) {
         int cont = 0;
-        for(int i=0; i<n1; i++) {
-            for(int j=0; j<n2; j++) {
-                if(alfa.charAt(i) != str.charAt(j)) {
+        for (int i = 0; i < n1; i++) {
+            for (int j = 0; j < n2; j++) {
+                if (alfa.charAt(i) == str.charAt(j)) {
                     cont++;
                 }
             }
         }
-        if(cont >= n2) {
-            System.out.println("N");
-        } else {
+        return cont == n2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        scanner.nextLine(); // Consumir o caractere de nova linha após a entrada dos números
+
+        String alfa = scanner.nextLine();
+        String str = scanner.nextLine();
+
+        boolean resposta = comparar(alfa, str, n1, n2);
+        if (resposta) {
             System.out.println("S");
+        } else {
+            System.out.println("N");
         }
-        sc.close();
+
+        scanner.close();
     }
 }
