e9ebe1e059842e6abbabf2587958ef13f7ef8c36
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-09-23 15:57:28 -0300

    Update jogo.java

--- jogo.java
+++ jogo.java
@@ -4,17 +4,13 @@ import java.util.Scanner;
 public class jogo {
     public static void main(String[] args) {
 
-        // Criar um scanner para entrada de dados
         Scanner sc = new Scanner(System.in);
 
-        // Gerar um número aleatório de 4 dígitos
         Random random = new Random();
         String randomNum = String.format("%04d", random.nextInt(10000));
         
         System.out.println("BORA JOGAR");
 
-        // Remova o comentário abaixo para ver o número aleatório durante testes
-        // System.out.println(randomNum); 
 
         for (int x = 0; x < 7; x++) {
             int tentativasRestantes = 7 - x;
@@ -24,21 +20,21 @@ public class jogo {
             System.out.println("Digite um número de 4 dígitos: ");
             String num = sc.nextLine();
 
-            // Verificar se o número digitado tem 4 dígitos
+            
             while (num.length() != 4) {
                 System.out.println("Esse número não é válido. Digite um número de 4 dígitos: ");
                 num = sc.nextLine();
             }
 
-            // Comparar o número digitado com o número aleatório
-            int corretosNaPosicao = 0; // Números na posição correta
-            int corretosForaDaPosicao = 0; // Números corretos, mas em posição errada
+            
+            int corretosNaPosicao = 0;
+            int corretosForaDaPosicao = 0; 
 
-            // Array para controlar se um dígito já foi utilizado
+            
             boolean[] usadoNoAleatorio = new boolean[4];
             boolean[] usadoNoInput = new boolean[4];
 
-            // Primeiro, verificar números na posição correta
+            
             for (int i = 0; i < 4; i++) {
                 if (num.charAt(i) == randomNum.charAt(i)) {
                     corretosNaPosicao++;
@@ -47,9 +43,9 @@ public class jogo {
                 }
             }
 
-            // Depois, verificar números corretos, mas em posições erradas
+            
             for (int i = 0; i < 4; i++) {
-                if (!usadoNoInput[i]) { // Se o dígito do input não foi usado
+                if (!usadoNoInput[i]) { 
                     for (int j = 0; j < 4; j++) {
                         if (!usadoNoAleatorio[j] && num.charAt(i) == randomNum.charAt(j)) {
                             corretosForaDaPosicao++;
@@ -62,7 +58,7 @@ public class jogo {
 
             if (corretosNaPosicao == 4) {
                 System.out.println("PARABÉNS, VOCÊ ACERTOU TODOS OS NÚMEROS E VENCEU O JOGO!!!");
-                System.exit(0); // Finaliza o jogo
+                System.exit(0); 
             } else {
                 System.out.println("Você tem " + corretosNaPosicao + " número(s) na posição correta.");
                 System.out.println("Você tem " + corretosForaDaPosicao + " número(s) correto(s) em posição(ões) errada(s).");
@@ -70,6 +66,6 @@ public class jogo {
         }
 
         System.out.println("Suas tentativas acabaram! O número correto era: " + randomNum);
-        sc.close(); // Fechar o scanner para evitar vazamentos de recurso
+        sc.close(); 
     }
 }
