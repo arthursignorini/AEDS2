e9d827f49903888b174aa3cd2107498cd80d36b4
Arthur Signorini Miranda <156106182+arthursignorini@users.noreply.github.com>
2024-10-07 15:21:14 -0300

    lista flexivel

---
+++ estruturasFlexiveis/listaFlexivel.java
@@ -0,0 +1,84 @@
+import java.util.*;
+
+public class listaFlexivel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Pilha pilha = new Pilha();
+        int x = 0;
+        while(x != 4) {
+            System.out.print("1) INSERIR  2) REMOVER  3) MOSTRAR: ");
+            x = sc.nextInt();
+            switch (x) {
+                case 1:
+                    System.out.print("Digite o numero que voce quer adicionar: ");
+                    int num = sc.nextInt();
+                    pilha.inserirPilha(num);
+                    break;
+
+                case 2:
+                    System.out.println("Elemento removido...");
+                    pilha.removerPilha();
+                    break;
+
+                case 3:
+                    System.out.println("Elementos da pilha:");
+                    pilha.mostrar();
+                    break;
+
+                case 4:
+                    System.out.println("Saindo...");
+                    break;
+    
+                default:
+                    System.out.println("Operação não é válida");
+                    break;
+            }
+        }  
+    }
+}
+
+class Celula {
+    public int elemento;
+    public Celula prox;
+
+    public Celula() {
+        this(0);
+    }
+    public Celula (int x) {
+        this.elemento = x;
+        this.prox = null;
+    }
+}
+
+class Pilha {
+    public Celula topo;
+
+    public Pilha() {
+        topo = null;
+    }
+    public void inserirPilha(int x) {
+        Celula tmp = new Celula(x);
+        tmp.prox = topo;
+        topo = tmp;
+        tmp = null;
+    }
+    public int removerPilha() {
+        if (topo == null) {
+            throw new NoSuchElementException("Erro ao remover: a pilha está vazia!");
+        }
+
+        int elemento = topo.elemento;
+        Celula tmp = topo;
+        topo = topo.prox;
+        tmp = null; 
+
+        return elemento;
+    }
+    public void mostrar() {
+        Celula i = topo;
+        while (i != null) {
+            System.out.println(i.elemento);
+            i = i.prox;
+        }
+    }
+}
